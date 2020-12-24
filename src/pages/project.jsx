43355a6b35f1a657a808020959985731ff6a0f84
Miros <miros@iden3.com>
2020-12-24 01:30:21 +0100

    adding deleted

---
+++ src/pages/project.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Project from '../components/project/project.component'
+
+const ProjectPage = () => {
+  return (
+    <Project />
+  )
+}
+
+export default ProjectPage
