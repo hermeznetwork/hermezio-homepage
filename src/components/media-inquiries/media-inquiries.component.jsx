1f3b83aa142cf3c98c8eb5ef7be2705e179fd645
Miros <miros@iden3.com>
2020-12-28 11:15:08 +0100

    removing TODO; minor fix;

--- src/components/media-inquiries/media-inquiries.component.jsx
+++ src/components/media-inquiries/media-inquiries.component.jsx
@@ -15,10 +15,11 @@ const MediaInquiries = () => {
         </div>
         <p className={classes.text}>
             Thank you for your interest in learning more about Hermez. For media inquiries, please email us at
-          <a className={classes.email} href='mailto:hello@hermez.io'> hello@hermez.io </a>
+          <a className={classes.email} href='mailto:hello@hermez.io'>
+            hello@hermez.io
+          </a>
             and click below to download our media kit for Hermez logos and illustrations kit.
         </p>
-        {/* TODO: add link to zip file, once it is ready */}
         <Button to='/hermez_mediakit.zip' text='Download Mediakit' hideIcon />
       </div>
     </section>
