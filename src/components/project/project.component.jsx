d1c7aae3bf3cae9fbbade6ab874ee9121ab2058e
Miros <miros@iden3.com>
2021-01-06 22:01:42 +0100

    copy change for product page

--- src/components/project/project.component.jsx
+++ src/components/project/project.component.jsx
@@ -27,11 +27,13 @@ const Project = () => {
         <section className={`${classes.section} ${classes.section2}`}>
           <div className={classes.row}>
             <div className={`${classes.col2} ${classes.title}`}>
-              <Title>Purpose</Title>
+              <Title>Development goals</Title>
             </div>
             <div className={classes.col2}>
-              <p>The idea about universal scaling solution comes from working on scaling decentralised identity with zero-knowledge proofs.</p>
-              <p>Hermez Network is expected to benefit from the advancements in identity infrastructure such as validation, private credentials and reputation for an improved usability and extend the range of potential use cases of a high throughput transaction network.</p>
+              <p>Everyone should have universal access to finance.</p>
+              <p>We are working on an open-source scalability model based on zero-knowledge technology (zk-rollup) to simplify and reduce costs for person-to-person payments, arbitrage opportunities and promote the adoption of digital currencies.</p>
+              <p>The decentralized Hermez protocol will provide a solid cryptographic foundation optimized for low-cost token transfers and enable future payment and financial solutions.</p>
+              <p>The development plan includes creating a private self-sovereign identity to support DeFi ecosystem.</p>
             </div>
           </div>
           <div className={classes.row}>
@@ -39,13 +41,13 @@ const Project = () => {
           </div>
           <div className={classes.row}>
             <div className={classes.col2}>
-              <Title>Core Values</Title>
+              <Title>Project qualities</Title>
             </div>
             <div className={classes.col2}>
               <div className={classes.subTitle}>
-                <Title type='h4'>Financial inclusion for all</Title>
+                <Title type='h4'>Production ready solution</Title>
               </div>
-              <p>Improving access to financial services and support entrepreneurship will bring significant social benefits and economic opportunities.</p>
+              <p>Hermez will launch a project in mainnet which is intended to be a usable token transfers network from day one. This product will have support, maintenance and technological evolution to provide users with the most updated functionalities with the right risk management.</p>
             </div>
           </div>
           <div className={classes.row}>
@@ -54,7 +56,7 @@ const Project = () => {
           <div className={classes.row}>
             <div className={classes.col2}>
               <div className={classes.subTitle}>
-                <Title type='h4'>Decentralisation</Title>
+                <Title type='h4'>Decentralized</Title>
               </div>
               <p>Hermez is called a network because the model is natively decentralized.</p>
               <p>It's a layer 2 construction and intends to be able to process thousands of transactions per second, so the consensus algorithm has to be simple for one agent (coordinator) to process this amount of transactions at any given time.</p>
@@ -67,20 +69,11 @@ const Project = () => {
           <div className={classes.row}>
             <div className={classes.col2}>
               <div className={classes.subTitle}>
-                <Title type='h4'>Security by design</Title>
-              </div>
-              <p>Hermez is a layer 2 construction that leverages Ethereum not only by using its native tokens, but also by borrowing its security as a strong public blockchain. Every Hermez Network batch can be securely reconstructed from the proof and data posted in the Ethereum Blockchain.</p>
-            </div>
-          </div>
-          <div className={classes.row}>
-            <span className={classes.divider} />
-          </div>
-          <div className={classes.row}>
-            <div className={classes.col2}>
-              <div className={classes.subTitle}>
-                <Title type='h4'>High efficiency</Title>
+                <Title type='h4'>Efficient</Title>
               </div>
               <p>The decentralized model is implemented through a permissionless auction system for potential coordinators of the network to earn the right to process transactions during a slot of time.</p>
+              <p>The auction model incentivizes the energy and cost efficiency of coordinators. They are pushed to process as many transactions as they can in order to collect the transaction fees, and to do so at the minimum operational cost, in order to be able to outbid other coordinators (and make money out of it).</p>
+              <p>Hermez implements a zk-rollup based on zk-SNARKs proofs, the most efficient cryptographic construction in terms of batch cost in Ethereum.</p>
             </div>
           </div>
           <div className={classes.row}>
@@ -89,10 +82,23 @@ const Project = () => {
           <div className={classes.row}>
             <div className={classes.col2}>
               <div className={classes.subTitle}>
-                <Title type='h4'>Focused on usability and integration</Title>
+                <Title type='h4'>Security in mind</Title>
               </div>
-              <p>Zk-rollups provide the technology for instant finality of transactions, which Hermez understands is a key feature for usability. Hermez protocol also provides a guarantee that user transactions can not be censored and that all funds can always be sent back to layer 1.</p>
-              <p>Layer 2 solutions increase complexity for the users, but the Hermez project focus is to provide the best possible usability for user onboarding, transactions and reduce friction and confusion between layers.</p>
+              <p>Hermez is a layer 2 construction that leverages Ethereum not only by using its native tokens, but also by borrowing its security as a strong public blockchain. Every Hermez Network batch can be securely reconstructed from the proof and data posted in the Ethereum Blockchain.</p>
+              <p>Hermez’ implementation is based in iden3’s own technology (Circom and SnarkJS libraries) and the cryptographic technology which has been proven as the most robust so far, as used in the ZCash blockchain.</p>
+            </div>
+          </div>
+          <div className={classes.row}>
+            <span className={classes.divider} />
+          </div>
+          <div className={classes.row}>
+            <div className={classes.col2}>
+              <div className={classes.subTitle}>
+                <Title type='h4'>Part of the community</Title>
+              </div>
+              <p>Hermez is designed to contribute to the community, since the project wouldn’t be possible without it. Innovations in scalability are a rare opportunity to realign incentives around the community and the public goods they provide.</p>
+              <p>40% of the generated value in the network will be sent to a donation mechanism designed to benefit the projects behind the ecosystem’s public goods.. It will start by contributing to Gitcoin Grants, but other pools might be enabled in the future.</p>
+              <p>Hermez will open source all the code of the protocol and the coordinator, is open to contributions, and has committed to contribute to the creation of a L2 ecosystem for initiatives such as a better L2 interoperability.</p>
             </div>
           </div>
         </section>
