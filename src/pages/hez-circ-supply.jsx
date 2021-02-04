8158df09b58418f8d4da2092bd971496d794ab7f
Miros <miros@iden3.com>
2021-02-04 10:40:39 +0100

    Circualting supply updated to 82500000

--- src/pages/hez-circ-supply.jsx
+++ src/pages/hez-circ-supply.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HezCircSupply = () => {
   return (
-    <pre class='total-hez' style={{ margin: '20% auto', textAlign: 'center' }}>5600000</pre>
+    <pre class='total-hez' style={{ margin: '20% auto', textAlign: 'center' }}>8250000</pre>
   )
 }
 
